ef5b8179140c97df6fac324f43d949803516bc26
Fededada23 <dadamofederico@gmail.com>
2023-01-09 14:08:45 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const {
+    createApp
+} = Vue
+// Array di oggetti
+createApp({
+    data(){
+        return{
+            emailList: [],
+            }
+        },
+        methods: {
+        },
+        mounted: function() {
+
+            for(let i = 0; i < 10; i++) {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result)=> {
+                    this.email = result.data.response;
+                    this.emailList.push(this.email);
+                });
+            }
+            
+        },
+    }    
+).mount('#app');
